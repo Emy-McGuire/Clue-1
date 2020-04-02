d93b4997bf51e836aaf179b53283efc687da0512
laurensciullo <laurensciullo@192.168.0.123>
2020-04-02 17:34:03 -0600

    Board & Runner
    Created the board and runner class

---
+++ Clue/src/PlayGame.java
@@ -0,0 +1,21 @@
+
+public class PlayGame {
+
+	public static void main(String[] args) 
+	{
+		//Assign player character
+		//Game selects person, place, and weapon then continues 
+		//Give character 3 cards (weapon, person, place); have to guess other cards 
+		//Ask which room go to? 
+		//Make suggesstions in rooms 
+			//Pull up list of people, weapons, and places 
+			//Ask if want to suggest person, place, or weapon 
+			//Player keeps track of wrong answers 
+		//Make accusation when ready 
+		
+	
+		Board.selectRoom();
+
+	}
+
+}
