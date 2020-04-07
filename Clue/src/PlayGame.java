49c6fcef313ad83cd4910c33c3a3bacd778988b7
emryn <emryn@10.0.0.187>
2020-04-07 14:44:01 -0600

    Made instructions

--- Clue/src/PlayGame.java
+++ Clue/src/PlayGame.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class PlayGame {
 
@@ -13,9 +14,37 @@ public class PlayGame {
 			//Player keeps track of wrong answers 
 		//Make accusation when ready 
 		
-	
+		intro();
 		Board.selectRoom();
+		
 
 	}
+	
+	
+	private static void intro() 
+	{
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Welcome to the Game of Clue! Do you want instructions? Yes (1) or No (2)?");
+		int choice = userInput.nextInt();
+		
+		if (choice == 1)
+		{
+			System.out.println("Players wander around the Clue game board mansion trying to solve the mystery of who was killed, by which weapon, and in what room. "
+					+ "Clue cards are given to each player but this is still not enough to solve the mystery. "
+					+ "Clue board game players begin making their accusations on who, where, and with what they think the murder took place. "
+					+ "If another player can prove them wrong by showing them one of his clue cards, the game continues and intensifies as more and more clues are given.");
+		
+		}
+		
+		else
+		{
+			System.out.println("");
+		}
+		
+		System.out.println("");
+		System.out.println("Let's get started!");
+	}
+
+	
 
 }
